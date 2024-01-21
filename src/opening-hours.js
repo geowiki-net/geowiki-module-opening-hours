adb14611c087095eb295a2b2001ef9ce1ad3ab43
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-01-21 08:51:28 +0100

    Move code from leaflet-geowiki

---
+++ src/opening-hours.js
@@ -0,0 +1,13 @@
+const OpeningHours = require('opening_hours')
+
+OverpassLayer.twig.extendFunction('openingHoursState', function (openingHours) {
+  try {
+    var oh = new OpeningHours(openingHours)
+  } catch (err) {
+    console.log('Error in opening_hours: ' + err)
+    return 'unknown'
+  }
+
+  return oh.getStateString(new Date(), true)
+})
+
